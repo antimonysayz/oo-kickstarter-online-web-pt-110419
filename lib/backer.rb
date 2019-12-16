4780a4e636414237af69ab35ee3e4aed7fa9751a
Anthony Mai <anthony.mai530@gmail.com>
2019-12-16 02:10:17 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -6,4 +6,9 @@ class Backer
     @backed_projects = []
   end
   
+  def back_project(project)
+    @backed_projects << project
+    project.add_backer(self) unless project.backers.include?(self)
+  end
+  
 end
